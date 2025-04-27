a2093f79bf9125bbc03c8316767cc90eddf044a3
phong_huynh <phong.huynh@zien.vn>
2025-04-27 17:27:11 +0700

    update social link

--- src/configs/author.ts
+++ src/configs/author.ts
@@ -1,5 +1,5 @@
 export const author = {
   email: 'htphongg96@gmail.com',
-  github: 'https://github.com/htphonggg',
+  github: 'https://github.com/phong-huynh',
   linkedIn: 'https://www.linkedin.com/in/htphong',
 }
