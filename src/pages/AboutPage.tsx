669a6f96f6201135e512c6980848c98d86c8faec
phong_huynh <phong.huynh@zien.vn>
2025-04-29 20:08:01 +0700

    add about page translation

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,16 @@
 import { memo } from 'react'
+import { useTranslation } from 'react-i18next'
 import * as Techs from '../assets'
 import AboutPic from '../assets/resizes/about.jpg'
 
 export const AboutPage = memo(() => {
+  const { t } = useTranslation()
+
   return (
     <section id="about" className="section-container about-page-container">
       <div className="d-flex flex-column align-items-center mb-3">
-        <p className="mb-0 section-sub-title">Good To Know More</p>
-        <h1 className="section-title">About Me</h1>
+        <p className="mb-0 section-sub-title">{t('good-to-know-more')}</p>
+        <h1 className="section-title">{t('about-me')}</h1>
       </div>
 
       <div className="about-page-content">
@@ -15,14 +18,7 @@ export const AboutPage = memo(() => {
           <img src={AboutPic} />
         </div>
         <div className="info-wrapper">
-          <p className="fs-5">
-            Software Engineer with 2.5+ years of experience building responsive
-            web and mobile applications using React.js and React Native with
-            strong interest and hands-on experience in Web3/Blockchain
-            integration. Proficient in modern JavaScript/TypeScript frameworks,
-            UI development and API integration. Comfortable collaborating with
-            backend systems and deploying full-stack solutions.
-          </p>
+          <p className="fs-5">{t('objectives')}</p>
 
           <div className="my-jobs-container">
             <div className="left-jobs">
@@ -34,13 +30,11 @@ export const AboutPage = memo(() => {
                     alt="mobile-phone"
                   />
                   <h5 className="mb-0 fw-bold">
-                    Mobile Applications Development
+                    {t('mobile-applications-development')}
                   </h5>
                 </div>
                 <p className="mb-0">
-                  Developed cross-platform mobile applications with a keen focus
-                  on crafting intuitive UI, flawless UX, adaptive, responsive
-                  designs and seamless integration with backend services.
+                  {t('mobile-applications-development-description')}
                 </p>
               </div>
 
@@ -51,13 +45,9 @@ export const AboutPage = memo(() => {
                     src={Techs.WebDevelopment}
                     alt="website-development"
                   />
-                  <h5 className="mb-0 fw-bold">Websites Development</h5>
+                  <h5 className="mb-0 fw-bold">{t('websites-development')}</h5>
                 </div>
-                <p className="mb-0">
-                  Building high-performance, user friendly websites with
-                  professional interfaces using cutting edge technologies for
-                  efficient and scalable web applications.
-                </p>
+                <p className="mb-0">{t('websites-development-description')}</p>
               </div>
             </div>
 
@@ -65,12 +55,9 @@ export const AboutPage = memo(() => {
               <div className="job-card">
                 <div className="d-flex mb-2 gap-3 align-items-center">
                   <img src={Techs.Blockchain} alt="blockchain" />
-                  <h5 className="mb-0 fw-bold">Blockchain</h5>
+                  <h5 className="mb-0 fw-bold">{t('blockchain')}</h5>
                 </div>
-                <p className="mb-0">
-                  Focused on creating secure, efficient blockchain products that
-                  fully leverage Smart Contracts and decentralized networks.
-                </p>
+                <p className="mb-0">{t('blockchain-description')}</p>
               </div>
 
               <div className="job-card">
@@ -80,13 +67,9 @@ export const AboutPage = memo(() => {
                     src={Techs.BackendDevelopment}
                     alt="backend-development"
                   />
-                  <h5 className="mb-0 fw-bold">Backend Development</h5>
+                  <h5 className="mb-0 fw-bold">{t('backend-development')}</h5>
                 </div>
-                <p className="mb-0">
-                  Focused on building high performance backend systems that
-                  handle heavy traffic, ensuring scalability and easy
-                  maintainability.
-                </p>
+                <p className="mb-0">{t('backend-development-description')}</p>
               </div>
 
               <div className="job-card">
@@ -96,12 +79,10 @@ export const AboutPage = memo(() => {
                     src={Techs.Education}
                     alt="backend-development"
                   />
-                  <h5 className="mb-0 fw-bold">Education</h5>
+                  <h5 className="mb-0 fw-bold">{t('education')}</h5>
                 </div>
-                <p className="mb-0">
-                  Software Engineering | Cao Thang Technical College
-                </p>
-                <p>Sep 2019 - Sep 2022 | GPA 7.6 / 10</p>
+                <p className="mb-0">{t('education-school')}</p>
+                <p>{t('education-gpa')}</p>
               </div>
             </div>
           </div>
